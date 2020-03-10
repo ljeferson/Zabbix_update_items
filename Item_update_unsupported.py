9b0bbaa8f23c4034007ac864b00e69d479f6c89c
ljeferson <lucasjeferson6000@gmail.com>
2020-03-10 12:24:24 -0300

    Atualização de itens não suportados: ok

---
+++ Item_update_unsupported.py
@@ -0,0 +1,40 @@
+from pyzabbix.api import ZabbixAPI
+import json
+
+zabbix = {}
+with open(f'conf.json', 'r') as confs:
+    zabbix = json.loads(confs.read())
+
+try:
+    zapi = ZabbixAPI(url=zabbix['url'], user=zabbix['username'], password=zabbix['password'])
+    print ('Auth Ok!')
+except Exception as err:
+    print('Erro ao conectar ao Zabbix')
+    print('Erro: {}'.format(err))
+
+getHostGroup = zapi.do_request('item.get',
+                            {
+                                "filter": {
+                                    "state": 1
+                                },
+                                "output": [
+                                    "hostid",
+                                    "name",
+                                    "itemid"
+                                ],
+                                "monitored": True
+                            }
+                        )
+i=0
+for item in getHostGroup["result"]:
+    i += 1
+    print(f"Item {i} --> {item}")
+
+
+for x in getHostGroup["result"]:
+    zapi.do_request('item.update',
+                    {
+                        "itemid": x['itemid'],
+                        "status": 1
+                    }
+                    )
